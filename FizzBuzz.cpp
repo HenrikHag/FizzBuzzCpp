ea7a4b78b6acb9f4860c34a8a2a5266d9db474c0
Henrik WinStasj <henrikbekhag@gmail.com>
2023-09-13 17:48:21 +0200

    Added random number generation

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <random>
 
 int main(int argc, char const *argv[])
 {
+    // Maximum number of rounds
     int countto = 16;
 
+    // Generate random numbers
+    std::random_device dev;
+    const uint_least32_t seed = dev();
+    std::mt19937 generator(seed);
+    std::uniform_real_distribution<double> distribute(0.0,1.0);
+
+    // Testing first random number
+    // std::cout << distribute(generator) << "\n";
+
+    // FizzBuzz loop
     for (int i = 1; i <= 16; ++i) {
         if (i%3==0 & i%5==0) {
             std::cout << "fizz bizz, ";
