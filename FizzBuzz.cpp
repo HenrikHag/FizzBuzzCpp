7ecbfffec2dafc6da264ae1c97f8701465121075
Henrik WinStasj <henrikbekhag@gmail.com>
2023-09-13 23:30:34 +0200

    Fixed random time delay on "responses"

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <random>
+#include <chrono>
+#include <thread>
 
 int main(int argc, char const *argv[])
 {
@@ -10,21 +12,27 @@ int main(int argc, char const *argv[])
     std::random_device dev;
     const uint_least32_t seed = dev();
     std::mt19937 generator(seed);
-    std::uniform_real_distribution<double> distribute(0.0,1.0);
+    std::uniform_int_distribution<uint32_t> distribute(50, 100);
 
     // Testing first random number
     // std::cout << distribute(generator) << "\n";
 
+    // Testing time delay
+    // std::this_thread::sleep_for(std::chrono::nanoseconds(2000000000));
+
     // FizzBuzz loop
     for (int i = 1; i <= 16; ++i) {
+        uint32_t timedelay = 10000000; // 0.01 sec
+        timedelay = timedelay * distribute(generator); // random delay
+        std::this_thread::sleep_for(std::chrono::nanoseconds(timedelay)); 
         if (i%3==0 & i%5==0) {
-            std::cout << "fizz bizz, ";
+            std::cout << "fizz bizz\n";
         } else if (i%3==0) {
-            std::cout << "fizz, ";
+            std::cout << "fizz\n";
         } else if (i%5==0) {
-            std::cout << "bizz, ";
+            std::cout << "bizz\n";
         } else {
-            std::cout << i << ", ";
+            std::cout << i << "\n";
         }
     }
     return 0;
--- TODO.md
+++ TODO.md
@@ -1,3 +1,6 @@
-- Add random time delay on response  
+TODO:  
 - Add game-stop if time delay passes some limit  
 - Add a Euler's FizzBuzz  
+
+DONE:  
+- Add random time delay on response (13.09.23)  
