c4fa91153e0e631deae8596aa7f65ea1ee08d69c
Henrik WinStasj <henrikbekhag@gmail.com>
2023-09-14 14:13:58 +0200

    Implemented "gamestop" and improved readability.

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -6,13 +6,13 @@
 int main(int argc, char const *argv[])
 {
     // Maximum number of rounds
-    int countto = 16;
+    int count_to = 50;
 
     // Generate random numbers
-    std::random_device dev;
-    const uint_least32_t seed = dev();
-    std::mt19937 generator(seed);
-    std::uniform_int_distribution<uint32_t> distribute(50, 100);
+    std::random_device rng_dev;
+    const uint_least32_t seed = rng_dev();
+    std::mt19937 rng_generator(seed);
+    std::uniform_int_distribution<uint32_t> uniform_distribute(50, 100);
 
     // Testing first random number
     // std::cout << distribute(generator) << "\n";
@@ -21,11 +21,15 @@ int main(int argc, char const *argv[])
     // std::this_thread::sleep_for(std::chrono::nanoseconds(2000000000));
 
     // FizzBuzz loop
-    for (int i = 1; i <= 16; ++i) {
-        uint32_t timedelay = 10000000; // 0.01 sec
-        timedelay = timedelay * distribute(generator); // random delay
-        std::this_thread::sleep_for(std::chrono::nanoseconds(timedelay)); 
-        if (i%3==0 & i%5==0) {
+    for (int i = 1; i <= count_to; ++i) {
+        auto rand_int = uniform_distribute(rng_generator);
+        if (rand_int == 100) {
+            std::cout << "Haha!! Too slow!\n";
+            break;
+        }
+        auto auto_timedelay = std::chrono::milliseconds(10 * rand_int); // random delay
+        std::this_thread::sleep_for(auto_timedelay);
+        if (i%3==0 && i%5==0) {
             std::cout << "fizz bizz\n";
         } else if (i%3==0) {
             std::cout << "fizz\n";
@@ -35,5 +39,6 @@ int main(int argc, char const *argv[])
             std::cout << i << "\n";
         }
     }
+    std::cout << "Game has ended!\n";
     return 0;
 }
--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
 TODO:  
-- Add game-stop if time delay passes some limit  
 - Add a Euler's FizzBuzz  
 
 DONE:  
+- Add game-stop if time delay passes some limit (14.09.23)  
 - Add random time delay on response (13.09.23)  
